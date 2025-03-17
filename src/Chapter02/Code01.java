0c409a082b1c5629c6271b033ac76fc686959c54
riftn7702 <riftn7702@naver.com>
2025-03-17 11:55:06 +0900

    main method 사용법, 연결 연산자와 덧셈 연산자의 차이점, println과 print의 차이점

--- src/Chapter02/Code01.java
+++ src/Chapter02/Code01.java
@@ -4,6 +4,6 @@ public class Code01 {
     public static void main(String[] args) {
         System.out.println("AI software\n");
         System.out.print('a'+'b');
-        System.out.println(""+100+200+300);
+        System.out.println(""+100+200+300+400);
     }
 }
