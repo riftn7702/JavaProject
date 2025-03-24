be27da6e9a2318696b72ea8fff7d29f8400df481
riftn7702 <riftn7702@naver.com>
2025-03-24 10:43:59 +0900

    사칙 계산기

---
+++ src/Chapter02/Code02_07.java
@@ -0,0 +1,20 @@
+package Chapter02;
+
+public class Code02_07 {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 50;
+
+        int result1 = num1 + num2;
+        System.out.printf("%d + %d = %d\n", num1, num2, result1);
+
+        int result2 = num1 - num2;
+        System.out.printf("%d - %d = %d\n", num1, num2, result2);
+
+        int result3 = num1 * num2;
+        System.out.printf("%d * %d = %d\n", num1, num2, result3);
+
+        int result4 = num1 / num2;
+        System.out.printf("%d / %d = %d\n", num1, num2, result4);
+    }
+}
